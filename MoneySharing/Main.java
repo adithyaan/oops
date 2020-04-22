9097a661881339fa00e05092d6523fb6e13f05bf
adithyaan <anirudhadi777@gmail.com>
2020-04-22 22:21:29 +0530

    version 1

--- MoneySharing/Main.java
+++ MoneySharing/Main.java
@@ -1,54 +1,86 @@
 import java.util.*;
+
+import javax.imageio.IIOException;
+
 import java.io.*;
 
 public class Main {
 
     int n = 3;
+    
     public static void main(String args[]) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        Map <String,Integer> give = new HashMap<>();
-        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+         BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+        Person A =  new Person("A");
+        Person B =  new Person("B");
+        Person  C =  new Person("C");
 
-        Person A = new Person();
-        Person B = new Person();
-        Person C = new Person();
-        Main main =  new Main();
 
         while(true) {
             String input  = reader.readLine();
 
             if(input.equals("n")){
+                findAmountToBeReceived(A,"A",B,C);
+                findAmountToBeReceived(B,"B",C,A);
+                findAmountToBeReceived(C,"C",B,A);
                 break;
             }
             String arr[] = input.split(" ");
 
             if(arr[1].equals("A")){
-                main.calcuate(A,arr);
+                calculate(A, arr);
             }
             else if(arr[1].equals("B")){
-                main.calculate(B,arr);
+                calculate(B,arr);
             }
             else if(arr[1].equals("C")){
-                main.calculate(C,arr);
+                calculate(C,arr);
             }
 
         }
-
     }
 
-     public void calculate(Person person,String arr[]){
+     static void calculate(Person person,String arr[]){
         int amount = Integer.parseInt(arr[0]);
         for(int i =2;i<arr.length;i++){
-            int value = person.spent.get(arr[i]);
             if(person.spent.containsKey(arr[i])) {
-
+                Map<String,Integer> spent = person.getSpent();
+                int value = spent.get(arr[i]);
                 int total=(amount / (arr.length - 1))+value;
-                person.spent.put(arr[i], total);
+                spent.put(arr[i], total);
+                person.setSpent(spent);
             }
             else{
                 person.spent.put(arr[i], amount / (arr.length - 1));
 
             }
         }
+    }
+    static void findAmountToBeReceived (Person person,String personName,Person one,Person two){
+        Map<String,Integer> spent = person.getSpent();
+        int spentFor=0;
+        int receivedFor=0;
+        Iterator hmIterator = spent.entrySet().iterator(); 
+
+        while (hmIterator.hasNext()) { 
+            Map.Entry mapElement = (Map.Entry)hmIterator.next(); 
+            spentFor += ((int)mapElement.getValue()); 
+        }
+         
+            int s1= one.getSpent().containsKey(personName) ? one.getSpent().get(personName) :0;
+            int s2= two.getSpent().containsKey(personName) ?two.getSpent().get(personName):0;
+            receivedFor = s1+s2;         
+
+        if(receivedFor == 0){
+            System.out.println(personName +"  gets  "+ spentFor);
+        }
+        else if(spentFor == 0){
+            System.out.println(personName +"  gives  "+ receivedFor);
+
+        }
+
+    }
+
+    static void findAmountToBeGiven(Person person,String personName,Person one,Person two){
+
     }
 }
--- MoneySharing/Person.java
+++ MoneySharing/Person.java
@@ -1,7 +1,33 @@
 import java.util.*;
 
 public class Person {
-    Map <String,Integer> spent = new HashMap<String,Integer>();
-    Map <String,Integer> received = new HashMap<String,Integer>();
+    public Map <String,Integer> spent = new HashMap<String,Integer>();
+    public Map <String,Integer> received = new HashMap<String,Integer>();
+    String personName;
+    public Person(String personName){
+        this.personName = personName;
+    }
+    
+    public String getPersonName() {
+        return personName;
+    }
+    
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+    public Map<String, Integer> getSpent() {
+        return spent;
+    }
 
+    public void setSpent(Map<String, Integer> spent) {
+        this.spent = spent;
+    }
+
+    public Map<String, Integer> getReceived() {
+        return received;
+    }
+
+    public void setReceived(Map<String, Integer> received) {
+        this.received = received;
+    }
 }
